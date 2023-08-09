5f43120988617cc49173d7f1f3d4dcc5581273ef
Adeleke Gabriel Tobi <themarveloustobi@gmail.com>
2023-08-09 20:17:39 +0100

    added weather app

---
+++ weather_app/weather/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+import json
+import urllib.request
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        req = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=d0729263232a9a67e9aa6757a63c1757').read()
+        json_data = json.loads(req)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + 'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        } 
+    else: 
+        data = {}
+    return render(request, 'index.html')
